21a2ecf22097f86522f23d0f9536950b0c44e707
Aiyappadas <aiy30403040@gmail.com>
2019-05-06 22:02:59 +0530

    new

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$(".tg_menu").click(function(){
+$(".content_left").toggleClass("current");
+$(".main").toggleClass("current");	
+$(".inner_sd").toggleClass("current");		
+});
+});
+
